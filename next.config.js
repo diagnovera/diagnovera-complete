b8a233617390171e8ca804cfe4c28ac2a14c2d13
Your Name <your.email@gmail.com>
2025-09-06 22:53:19 -0700

    Move Next.js app to root for Vercel

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000',
+  typescript: {
+    ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
   },
 }
 
